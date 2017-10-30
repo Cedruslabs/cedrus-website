4490f8aac629338d1214c8f4856b4e61a97b783e
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2017-10-30 20:26:00 +0000

    Update favicon and site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'cedrus-website',
+    title: 'Cedrus',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Cedrus web agency. Using, the latest technologies to build bulletproof applications.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
